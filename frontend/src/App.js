bca37c3b7df4b45390e9fe22d330dbd6486434ae
randiandn29 <60083999+randiandn29@users.noreply.github.com>
2021-12-01 23:59:35 +0700

    cardview

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
-import './styles/output.css'
-import './index.css';
-import "tailwindcss/tailwind.css"
+
 import Header from './component/header/Header';
 import Login from './view/login/Login';
 import About from './view/About';
 import Signup from './view/Signup'
 import Utama from './view/Utama';
+import CardView from './component/CardView';
 import { makeStyles } from "@material-ui/core/styles";
 import LengkapiData from './view/LengkapiData1';
 
@@ -19,13 +18,13 @@ export default function App(){
     <div className={classes.container}>
       <Router>
           <Header />
-        <Switch>
+        {/* <Switch>
           <Route exact from="/" render={props => <Utama {...props} />} />
           <Route exact from="/login" render={props => <Login {...props} />} />
           <Route exact from="/signup" render={props => <Signup {...props} />} />
           <Route exact from="/about" render={props => <About {...props} />} />
-        </Switch>
-        {/* <LengkapiData/> */}
+        </Switch> */}
+        <CardView/>
       </Router>
       
     </div>
